62745f9edf2289ae5bb95189cf26b201e392803f
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2017-08-02 18:11:38 -0400

    first commit
    add babelrc configuration
    add rollup configuration
    add eslint

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import babel from "rollup-plugin-babel"
+import pkg from "./package.json"
+
+export default [
+  // UMD Build (Browser)
+  {
+    entry: "src/index.js",
+    dest: pkg.browser,
+    format: "umd",
+    moduleName: "GPRoutine",
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel(),
+    ],
+  },
+
+  // CommonJS and ES6 Module
+  {
+    entry: "src/index.js",
+    targets: [
+      { dest: pkg.main, format: "cjs" },
+      { dest: pkg.module, format: "es" },
+    ],
+    plugins: [
+      babel(config),
+    ],
+  }
+]
