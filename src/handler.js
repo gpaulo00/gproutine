14ab39818523ba4be558015f1e7e8bd8b5d41899
Gustavo Paulo <gustavo.paulo.segura@gmail.com>
2017-08-03 16:30:37 -0400

    add bugfixes
    - remove nested function
    - add error handling
    - add some documentation

--- src/handler.js
+++ src/handler.js
@@ -5,48 +5,72 @@ import {
   isGeneratorFunction, isEffect,
 } from "./helpers"
 
-export default function handler(iterator) {
-  const i = isGenerator(iterator) ? iterator : iterator()
-  const iter = i.next()
-  if (iter.done) {
-    return Promise.resolve(iter.value)
+/**
+ * This function handles ES2015 generator functions to work.
+ * @param  {Generator} iterator ES2015 Generator to handle
+ * @param  {any} old  Result of the previous iteration
+ * @return {Promise}      Runtime Promise
+ */
+export default function handler(iterator, old) {
+  if (!isGenerator(iterator) && !isGeneratorFunction(iterator)) {
+    throw new Error("the argument isn't a generator")
   }
-  return nested(i, iter.value)
-}
+  const iter = isGenerator(iterator) ? iterator : iterator()
 
-export function nested(iter, old) {
-  function callback(gen, i, resolve) {
+  function callback(gen, i, resolve, reject) {
     if (!i.done) {
-      nested(gen, i.value).then(resolve)
+      handler(gen, i.value).then(resolve).catch(reject)
     } else {
       resolve(i.value)
     }
   }
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    const args = [resolve, reject]
+
     toPromise(old)
       .then((res) => {
         const i = iter.next(res)
-        callback(iter, i, resolve)
+        callback(iter, i, ...args)
       })
       .catch((err) => {
-        const i = iter.throw(err)
-        callback(iter, i, resolve)
+        try {
+          const i = iter.throw(err)
+          callback(iter, i, ...args)
+        } catch (error) {
+          reject(error)
+        }
       })
   })
 }
 
+/**
+ * Convert anything to a Promise.
+ *
+ * This function can handle:
+ * - Promises
+ * - Generator (delegation to **nested**)
+ * - Generators Functions (delegation to **handler**)
+ * - Arrays of Promises (using **Promise.all**)
+ * - Effects (running them)
+ * @param  {any} obj Value to convert
+ * @return {Promise}     Result
+ */
 export function toPromise(obj) {
   if (!obj) return Promise.resolve(obj)
   if (isPromise(obj)) return obj
-  if (isGenerator(obj)) return nested(obj)
-  if (isGeneratorFunction(obj)) return handler(obj)
+  if (isGenerator(obj) || isGeneratorFunction(obj)) return handler(obj)
   if (Array.isArray(obj)) return Promise.all(obj.map(toPromise, this))
 
   if (isEffect(obj)) return handleEffect(obj)
   return Promise.resolve(obj)
 }
 
+/**
+ * Handles an effect
+ * @param  {object} effect The effect to handle
+ * @return {Promise}        Result
+ */
 export function handleEffect(effect) {
   const action = actions.get(effect.type)
   if (!action) return Promise.reject("action not defined")
